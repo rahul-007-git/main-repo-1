40808e30d10a826d404c1f5f5a5f90f73c693bfd
nithyasri sk <mail2rahul.mk@gmail.com>
2025-07-04 12:00:54 +0530

    finishe

---
+++ import matplotlib.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+name = "STEEL BOT BOY"
+
+fig, ax = plt.subplots()
+ax.axis('off') 
+
+text = ax.text(0.5, 0.5, '', fontsize=30, color='red',
+               fontweight='bold', family='cursive', ha='center', va='center')
+
+def update(frame):
+    text.set_text(name[:frame])  
+    return text,
+ani = animation.FuncAnimation(
+    fig, update, frames=len(name)+1, interval=300, repeat=False
+)
+
+plt.show()
